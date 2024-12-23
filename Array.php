3be0b82e083160a287037e0e9b630931e661d030
Vert San <itsanvert@gmail.com>
2024-12-23 18:11:12 +0700

    first commit

---
+++ Array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array</title>
+</head>
+
+<body>
+    <?php
+    // Array contract
+    $users = array("Vert", "Sok", "Meta", "Karona");
+    echo "This array of 0 is " . $users[0];
+    echo "<br/>";
+    // Array identifier
+
+    $user[] = "Sok";
+    $user[] = "Dara";
+    $user[] = "Vert";
+    echo "This array of 0 is ".$user[0];
+    ?>
+</body>
+
+</html>
